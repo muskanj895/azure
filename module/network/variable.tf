4139aeba02c585232b7f4f2c058890034387642c
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-08-05 13:26:33 +0530

    Create variable.tf

---
+++ module/network/variable.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+
+}
+variable "location" {
+
+}
+
+variable "vnet" {
+default = "mj"
+}
+variable "address_space" {
+default = "10.0.0.0/16"
+}
+ variable "subnet_name" {
+  default = ["subnet1","subnet2"]
+ }
+variable "subnet_prefix" {
+ default = ["10.0.1.0/24","10.0.2.0/24"]
+}

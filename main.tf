45911ad5d819d2dc73b1119cba78891d426e923a
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-08-06 00:42:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,10 @@ resource_group_name = "module.rg.name"
 location = "eastus"
 depends_on = [module.rg.azurerm_resource_group]
 }
+module "vm" {
+source = "./module/vm"
+resource_group_name = module.rg.resource_group_name1
+location = "eastus"
+subnet = module.network.subnet_id
+security_group_id = module.network.security_group_id
+}

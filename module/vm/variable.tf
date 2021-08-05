5c94c9c87869b89970adc88138f5fdaf7fb53b9e
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-08-06 00:39:16 +0530

    Create variable.tf

---
+++ module/vm/variable.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+
+}
+variable "location" {
+
+}
+
+variable "vnet" {
+default = "mj"
+}
+variable "ipname" {
+default = "mypublicip"
+}
+variable "nicname" {
+default = "myNIC"
+}
+variable "subnet" {
+}
+variable "security_group_id" {
+}

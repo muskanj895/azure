497684945912b8d2ef386d68d576eff405398cdb
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-08-12 18:22:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,24 @@
-provider "azurerm" {
-  features {}
+variable "address_space" {
+default = "10.0.0.0/16"
+}
+ variable "subnet_name" {
+  default = ["subnet1","subnet2"]
+ }
+variable "subnet_prefix" {
+default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+variable "resource_group" {
+   default = "myresource"
+}
+variable "location" {
+default = "eastus"
+}
+variable "vnet" {
+default = "mj"
+}
+variable "ipname" {
+default = "mypublicip"
+}
+variable "nicname" {
+default = "myNIC"
 }

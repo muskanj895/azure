22de0a5a494a38b87ada3c0ecc2108bc24bdfd59
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-08-12 18:22:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
+provider "azurerm" {
+  features {}
+}
 module "rg" {
 source = "./module/rg"
+resource_group ="${var.resource_group}"
+location = "eastus"
 }
-
 module "network"{
 source = "./module/network"
-resource_group_name = "module.rg.name"
+resource_group_name = module.rg.resource_group_name1
 location = "eastus"
-depends_on = [module.rg.azurerm_resource_group]
+vnet = "${var.vnet}"
+subnet_name = "${var.subnet_name}"
+address_space ="${var.address_space}"
+subnet_prefix = "${var.subnet_prefix}"
 }
 module "vm" {
 source = "./module/vm"
@@ -14,4 +21,9 @@ resource_group_name = module.rg.resource_group_name1
 location = "eastus"
 subnet = module.network.subnet_id
 security_group_id = module.network.security_group_id
+ipname = "${var.ipname}"
+vnet = "${var.vnet}"
+nicname = "${var.nicname}"
 }
+
+

5ca92340412859c7c5ff532b9899fff512035d6a
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-08-12 18:25:00 +0530

    Update output.tf

--- module/network/output.tf
+++ module/network/output.tf
@@ -3,4 +3,8 @@ value = "${azurerm_subnet.example[0].id}"
 }
 output "security_group_id" {
 value ="${azurerm_network_security_group.tamopsnsg.id}"
+
+}
+output "vnet_id" {
+value = "${azurerm_virtual_network.example.id}"
 }

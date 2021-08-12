470677585fad91eaa6a045982419319d9cddac24
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-08-12 18:28:33 +0530

    Update variable.tf

--- module/vm/variable.tf
+++ module/vm/variable.tf
@@ -1,18 +1,12 @@
 variable "resource_group_name" {
-
 }
 variable "location" {
-
 }
-
 variable "vnet" {
-default = "mj"
 }
 variable "ipname" {
-default = "mypublicip"
 }
 variable "nicname" {
-default = "myNIC"
 }
 variable "subnet" {
 }
